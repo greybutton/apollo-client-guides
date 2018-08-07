a6046ced26a1e9a906a77bf420634be2381af781
greybutton <greybutton@yandex.com>
2018-08-07 14:43:01 +0800

    Local state management

--- src/index.js
+++ src/index.js
@@ -1,126 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from "apollo-boost";
-import gql from "graphql-tag";
-import { ApolloProvider, Query, Mutation } from "react-apollo";
+import { ApolloProvider } from "react-apollo";
+
+import App from './components/App';
+import { defaults, resolvers } from "./resolvers";
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 const client = new ApolloClient({
-  uri: "https://8v9r9kpn7q.lp.gql.zone/graphql"
+  uri: `https://nx9zvp49q7.lp.gql.zone/graphql`,
+  clientState: {
+    defaults,
+    resolvers,
+  }
 });
-
-const ADD_TODO = gql`
-  mutation AddTodo($type: String!) {
-    addTodo(type: $type) {
-      id
-      type
-    }
-  }
-`;
-
-const GET_TODOS = gql`
-  {
-    todos {
-      id
-      type
-    }
-  }
-`;
-
-const UPDATE_TODO = gql`
-  mutation UpdateTodo($id: String!, $type: String!) {
-    updateTodo(id: $id, type: $type) {
-      id
-      type
-    }
-  }
-`;
-
-const AddTodo = () => {
-  let input;
-
-  return (
-    <Mutation 
-      mutation={ADD_TODO}
-      update={(cache, { data: { addTodo } }) => {
-        const { todos } = cache.readQuery({ query: GET_TODOS });
-        cache.writeQuery({
-          query: GET_TODOS,
-          data: { todos: todos.concat([addTodo]) }
-        });
-      }}
-    >
-      {addTodo => (
-        <div>
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-              addTodo({ variables: { type: input.value } });
-              input.value = "";
-            }}
-          >
-            <input
-              ref={node => {
-                input = node;
-              }}
-            />
-            <button type="submit">Add Todo</button>
-          </form>
-        </div>
-      )}
-    </Mutation>
-  );
-};
-
-const Todos = () => (
-  <Query query={GET_TODOS}>
-    {({ loading, error, data }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error :(</p>;
-
-      return data.todos.map(({ id, type }) => {
-        let input;
-
-        return (
-          <Mutation mutation={UPDATE_TODO} key={id}>
-            {(updateTodo, { loading, error }) => (
-              <div>
-                <p>{type}</p>
-                <form
-                  onSubmit={e => {
-                    e.preventDefault();
-                    updateTodo({ variables: { id, type: input.value } });
-
-                    input.value = "";
-                  }}
-                >
-                  <input
-                    ref={node => {
-                      input = node;
-                    }}
-                  />
-                  <button type="submit">Update Todo</button>
-                </form>
-                {loading && <p>Loading...</p>}
-                {error && <p>Error :( Please try again</p>}
-              </div>
-            )}
-          </Mutation>
-        );
-      });
-    }}
-  </Query>
-);
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <div>
-      <h2>Building Mutation components 🚀</h2>
-      <AddTodo />
-      <Todos />
-    </div>
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 );
